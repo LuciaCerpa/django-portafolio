2a0c9347a369609c59471eb849387551891f3713
lucy1528 <luxysm@gmail.com>
2022-10-06 14:17:21 -0500

    database conection

--- django_portafolio/settings.py
+++ django_portafolio/settings.py
@@ -85,10 +85,13 @@ WSGI_APPLICATION = 'django_portafolio.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': dj_database_url.config(
+    #      default='postgresql://postgres:postgres@localhost:5432/mysite', conn_max_age=600
+    # )}
     'default': dj_database_url.config(
-         default='postgresql://postgres:postgres@localhost:5432/mysite', conn_max_age=600
+         default='postgresql://postgres:h4mGuY4NCeghDxqzCRs4@containers-us-west-50.railway.app:5602/railway', conn_max_age=600
     )}
-    #'default': {
+    # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
